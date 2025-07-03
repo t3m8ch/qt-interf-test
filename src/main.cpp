cb2eb2304dcdda1fc8d5918902b061f10575ed8b
Artem Kudyakov <t3m8ch@yandex.ru>
2025-07-03 16:06:48 +0400

    refactor: Rename main.qml to WeatherWindow.qml

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
   QQmlApplicationEngine engine;
-  const QUrl url(QStringLiteral("qrc:/qt/qml/QtInterfTest/qml/main.qml"));
+  const QUrl url(
+      QStringLiteral("qrc:/qt/qml/QtInterfTest/qml/WeatherWindow.qml"));
 
   QObject::connect(
       &engine, &QQmlApplicationEngine::objectCreated, &app,
